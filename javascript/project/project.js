a8a7dab71ad52698971a9f39076361fa244841db
vhande <karasu.hnd@gmail.com>
2022-06-17 16:23:47 +0200

    project

---
+++ javascript/project/project.js
@@ -0,0 +1,87 @@
+const houses = [
+    {id: 1,
+    pand: "huis",
+    prijs: 350.000,
+    oppervlak: 200,
+    stad: "Brussel",
+    kamers: 2,
+    tuin: true},
+
+    {id: 2,
+    pand: "appartement",
+    prijs: 150.000,
+    oppervlak: 100,
+    stad: "Gent",
+    kamers: 1,
+    tuin: false},
+    
+    {id: 3,
+    pand: "huis",
+    prijs: 700.000,
+    oppervlak: 500,
+    stad: "Antwerpen",
+    kamers: 4,
+    tuin: true},
+
+    {id: 4,
+    pand: "appartement",
+    prijs: 300.000,
+    oppervlak: 150,
+    stad: "Gent",
+    kamers: 3,
+    tuin: false},
+
+    {id: 5,
+    pand: "huis",
+    prijs: 500.000,
+    oppervlak: 350,
+    stad: "Brussel",
+    kamers: 4,
+    tuin: false},
+
+    {id: 6,
+    pand: "appartement",
+    prijs: 250.000,
+    oppervlak: 100,
+    stad: "Brussel",
+    kamers: 1,
+    tuin: true},
+
+
+    {id: 7,
+    pand: "huis",
+    prijs: 375.000,
+    oppervlak: 300,
+    stad: "Antwerpen",
+    kamers: 2,
+    tuin: true},
+
+    {id: 8,
+    pand: "appartement",
+    prijs: 400.000,
+    oppervlak: 250,
+    stad: "Antwerpen",
+    kamers: 3,
+    tuin: false},
+
+    {id: 9,
+    pand: "huis",
+    prijs: 150.000,
+    oppervlak: 100,
+    stad: "Brussel",
+    kamers: 1,
+    tuin: false},
+
+    {id: 10,
+    pand: "appartement",
+    prijs: 330.000,
+    oppervlak: 240,
+    stad: "Gent",
+    kamers: 2,
+    tuin: true},
+    
+
+  
+    
+
+      ]
