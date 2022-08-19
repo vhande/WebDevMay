14f09811479e1333d1f5d71f70e170fe9fa5a9d3
vhande <karasu.hnd@gmail.com>
2022-08-19 17:52:50 +0200

    lastupdate

--- reactjs/tutorial2/src/index.js
+++ reactjs/tutorial2/src/index.js
@@ -1,11 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import 'bootstrap/dist/css/bootstrap.min.css';
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 
+import App from "./App";
+
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
